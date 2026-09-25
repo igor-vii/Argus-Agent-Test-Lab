ee6d742780f9633e02ced022203d1e68e3b54d05
igor-vii <auvip@mail.ru>
2026-09-25 14:43:24 +0000

    Remove node_modules/ and dist/ from tracking; restore .gitignore from main

--- dist/scenarios/S8_X402Payment.d.ts
+++
@@ -1,3 +0,0 @@
-import { ScenarioDefinition } from '../core/ScenarioDefinition';
-export declare const S8_X402Payment: ScenarioDefinition;
-//# sourceMappingURL=S8_X402Payment.d.ts.map
--- dist/tests/adapters/x402/X402AgentAdapter.test.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=X402AgentAdapter.test.d.ts.map
